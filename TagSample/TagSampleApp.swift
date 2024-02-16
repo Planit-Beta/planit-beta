31ec26923e50bf1c9f5477ef8908034b5b1a22c1
Haruto Hamano <haruto.yhs318@gmail.com>
2024-02-16 11:58:06 +0900

    Initial Commit

---
+++ TagSample/TagSampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  TagSampleApp.swift
+//  TagSample
+//
+//  Created by 濱野遥斗 on 2024/02/16.
+//
+
+import SwiftUI
+
+@main
+struct TagSampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
