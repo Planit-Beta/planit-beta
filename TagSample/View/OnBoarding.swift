58076bd73a1ccd2cf43e7b8dcb3b537c4a551454
Haruto Hamano <haruto.yhs318@gmail.com>
2024-02-17 00:37:06 +0900

    add zen maru font

--- TagSample/View/OnBoarding.swift
+++ TagSample/View/OnBoarding.swift
@@ -13,7 +13,7 @@ struct OnBoarding: View {
     var body: some View {
         VStack{
             VStack(alignment: .leading){ ///画面上部のバー
-                Text("新しく旅程を作成する").padding().foregroundStyle(Color(UIColor(hexString: "333333")))
+                Text("新しく旅程を作成する").padding().font(.custom("ZenMaruGothic-Medium", size: 20.0)).foregroundStyle(Color(UIColor(hexString: "333333")))
                 ProgressView(value: progressVal, total: 100)
                     .animation(.easeInOut, value: progressVal)//バーのアニメーション
                     .tint(Color(UIColor(hexString: "333333")))//バーの色
@@ -47,70 +47,66 @@ struct OnBoarding: View {
                         Color(Color(UIColor(hexString: "FFFFFF")))
                     )
                     .ignoresSafeArea(.container, edges: .all)
-                
-                VStack {///画面下部の矢印ボタン
-                    HStack(spacing: 50) {
-                        Spacer()
-                        Button {
-                            backToPriveousPage()
-                            progressVal -= 50
-                            selection -= 1
-                        } label: {
-                            Image(systemName: "arrowtriangle.backward.fill")
-                                .foregroundColor(Color(UIColor(hexString: "9C9C9C")))
-                                .font(.system(size: 50))
-                            
-                        }
-                        .disabled(!isShowBackBtn)
-                        .opacity(isShowBackBtn ? 1 : 0)
-                        .animation(.easeInOut, value: getIndex())
+            }.padding()
+            
+            VStack {///画面下部の矢印ボタン
+                HStack(spacing: 50) {
+                    Spacer()
+                    Button {
+                        backToPriveousPage()
+                        progressVal -= 50
+                        selection -= 1
+                    } label: {
+                        Image(systemName: "arrowtriangle.backward.fill")
+                            .foregroundColor(Color(UIColor(hexString: "9C9C9C")))
+                            .font(.system(size: 50))
                         
-                        HStack(spacing: 20) {
-                            ForEach(boardingScreens.indices, id: \.self) { index in
-                                
-                                Circle()
-                                    .fill(Color(UIColor(hexString: "9C9C9C")))
-                                    .opacity(index == getIndex() ? 1 : 0.4)
-                                    .frame(width: 12, height: 12)
-                                    .scaleEffect(index == getIndex() ? 1.2 : 1)
-                                    .animation(.easeInOut, value: getIndex())
-                            }
-                        }
-                        .frame(maxWidth: .infinity)
-                        
-                        
-                        Button {
-                            goToNextPage()
-                            progressVal += 50
-                            selection += 1
-                        } label: {
-                            Image(systemName: "arrowtriangle.forward.fill")
-                                .foregroundColor(Color(UIColor(hexString: "9C9C9C")))
-                                .font(.system(size: 50))
-                            
-                        }
-                        .disabled(!isShowNextBtn)
-                        .opacity(isShowNextBtn ? 1 : 0)
-                        .animation(.easeInOut, value: getIndex())
-                        Spacer()
                     }
-                    .padding(.top, 25)
+                    .disabled(!isShowBackBtn)
+                    .opacity(isShowBackBtn ? 1 : 0)
+                    .animation(.easeInOut, value: getIndex())
+                    
+                    HStack(spacing: 20) {
+                        ForEach(boardingScreens.indices, id: \.self) { index in
+                            
+                            Circle()
+                                .fill(Color(UIColor(hexString: "9C9C9C")))
+                                .opacity(index == selection-1 ? 1 : 0.4)
+                                .frame(width: 12, height: 12)
+                                .scaleEffect(index == selection-1 ? 1.2 : 1)
+                                .animation(.easeInOut, value: selection)
+                        }
+                    }
+                    .frame(maxWidth: .infinity)
+                    
+                    
+                    Button {
+                        goToNextPage()
+                        progressVal += 50
+                        selection += 1
+                    } label: {
+                        Image(systemName: "arrowtriangle.forward.fill")
+                            .foregroundColor(Color(UIColor(hexString: "9C9C9C")))
+                            .font(.system(size: 50))
+                        
+                    }
+                    .disabled(!isShowNextBtn)
+                    .opacity(isShowNextBtn ? 1 : 0)
+                    .animation(.easeInOut, value: getIndex())
+                    Spacer()
                 }
-                
+                .padding(.top, 25)
             }
-                
-                
-                
-            }
+        }
     }
     
-    func getRotation() -> Double {
-        let process = offset / (getScreenBounds().width * 4)
-        
-        let rotation = Double(process) * 360
-        
-        return rotation
-    }
+//    func getRotation() -> Double {
+//        let process = offset / (getScreenBounds().width * 4)
+//        
+//        let rotation = Double(process) * 360
+//        
+//        return rotation
+//    }
     
     func getIndex() -> Int {
         
@@ -158,13 +154,23 @@ struct OnBoarding_Previews: PreviewProvider {
 
 struct PageOneView: View {
     var body: some View {
+        @State var inputName = ""
 
         VStack{
 //            Color.black
-            Text("行き先とそこに行きたい時間帯を\n入力してください").font(.custom("ZenMaruGothic-Regular", size: 20.0)).foregroundStyle(Color(UIColor(hexString: "333333")))
+            Text("行き先とそこに行きたい時間帯を入力してください").font(.custom("ZenMaruGothic-Regular", size: 20.0)).foregroundStyle(Color(UIColor(hexString: "333333")))
             Spacer()
+            
             VStack(spacing: 10){
-                Text("旅行名").font(.custom("ZenMaruGothic-Regular", size: 20.0)).foregroundStyle(Color(UIColor(hexString: "333333")))
+                HStack{
+                    Text("旅行名").font(.custom("ZenMaruGothic-Regular", size: 20.0)).foregroundStyle(Color(UIColor(hexString: "333333")))
+                    Spacer()
+                }
+                
+                TextField("旅程名を入力してください", text: $inputName)
+                    .padding()
+                    .font(.custom("ZenMaruGothic-Regular", size: 12.0)).foregroundStyle(Color(UIColor(hexString: "FFFFFF"))).background(Color(UIColor(hexString: "D9D9D9")))
+                    .cornerRadius(10)
                 
             }
             Spacer()
