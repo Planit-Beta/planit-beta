744f8ccb1490eaccb2af219f11b3eed6937a70c6
Haruto Hamano <haruto.yhs318@gmail.com>
2024-02-21 21:22:33 +0900

    completed UI for PageThreeView

--- TagSample/View/page3/PageThreeView.swift
+++ TagSample/View/page3/PageThreeView.swift
@@ -10,55 +10,66 @@ import SwiftUI
 struct PageThreeView: View {
     @State private var pageTwoInputName: String = ""
     
+    @State var situation: [Tag] = situationTags.map { Tag(name: $0) }
+    
     var body: some View {
-        ScrollView(showsIndicators: false){
-            VStack(spacing: 10){
-                Text("こちらの内容でよろしいですか").font(.custom("ZenMaruGothic-Regular", size: 20.0)).foregroundStyle(Color(UIColor(hexString: "333333")))
-                
+        VStack(spacing: 10){
+            VStack(spacing: 20){
+                Text("その他の項目を選択してください").font(.custom("ZenMaruGothic-Regular", size: 20.0)).foregroundStyle(Color(UIColor(hexString: "333333")))
                 Spacer()
                 
-                VStack(spacing: 10){
-                    HStack{
-                        Text("行き先").font(.custom("ZenMaruGothic-Regular", size: 20.0)).foregroundStyle(Color(UIColor(hexString: "333333")))
-                        Spacer()
-                    }
-                    
-                    CardView(image: Image("sample_\(1)"), size: 210)
-                }
-                
-                Spacer()
-                
-                VStack(spacing: 10){
-                    HStack{
-                        Text("シチュエーション")
+                VStack(spacing: 25){
+                    HStack(spacing: 20){
+                        Text("場所")
+                            .font(.custom("ZenMaruGothic-Regular", size: 20.0))
+                            .opacity(0.5)
+                            .foregroundStyle(Color(UIColor(hexString: "333333")))
+                        Text("現在地から")
                             .font(.custom("ZenMaruGothic-Regular", size: 20.0))
                             .foregroundStyle(Color(UIColor(hexString: "333333")))
                         Spacer()
                     }
-                    
-                    TagForThreeView()
+                    HStack(spacing: 20){
+                        Text("時間")
+                            .font(.custom("ZenMaruGothic-Regular", size: 20.0))
+                            .opacity(0.5)
+                            .foregroundStyle(Color(UIColor(hexString: "333333")))
+                        Text("現在時刻から4時間-6時間")
+                            .font(.custom("ZenMaruGothic-Regular", size: 20.0))
+                            .foregroundStyle(Color(UIColor(hexString: "333333")))
+                        Spacer()
+                    }
                 }
+                Spacer()
                 
+                VStack(spacing: 10){
+                    HStack{
+                        Text("その他")
+                            .font(.custom("ZenMaruGothic-Regular", size: 20.0))
+                            .foregroundStyle(Color(UIColor(hexString: "333333")))
+                        Spacer()
+                    }
+                    ToggleTagView(tags: $situation)
+                }
+
                 Spacer()
                 
                 Button {
                     
                 } label: {
                     Text("旅程を生成する")
-                        .font(.custom("ZenMaruGothic-Regular", size: 24.0))
-                        .foregroundStyle(Color(UIColor(hexString: "333333")))
+                        .font(.custom("ZenMaruGothic-Medium", size: 24.0))
+                        .foregroundStyle(Color(UIColor(hexString: "FFFFFF")))
                         .frame(width: 320, height: 80)
-                        .background(Color(UIColor(hexString: "F5F5F5")))
+                        .background(Color(UIColor(hexString: "F8714F")))
                         .cornerRadius(20)
                         .shadow(color: .gray, radius: 2, x: 0, y: 2)
                 }
                 .padding(.vertical)
-                
                 Spacer()
-            }
-            
-        }
-        .padding()
+                
+            }.padding(.horizontal)
+        }.padding()
     }
 }
 
