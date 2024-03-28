bf568583821ab6d0a50c91cfa3fdda3daa16a9cb
Haruto Hamano <haruto.yhs318@gmail.com>
2024-03-28 23:57:09 +0900

    created main view of register view

--- TagSample/Login/RegisterView.swift
+++ TagSample/Login/RegisterView.swift
@@ -8,8 +8,44 @@
 import SwiftUI
 
 struct RegisterView: View {
+    @Environment(\.dismiss) var dismiss
+    
+    @State var isMoveToHome: Bool = false
+    
+    @State var inputEmail: String = ""
+    @State var inputPassword: String = ""
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        ZStack{
+            Color(UIColor(hexString: "FDF5F3")).ignoresSafeArea()
+            
+            VStack(spacing: 80){
+                VStack(spacing: 20){
+                    Text("新規登録").foregroundStyle(Color(UIColor(hexString: "333333"))).font(.custom("ZenMaruGothic-Medium", size: 20))
+                    Divider().frame(width: 240, height: 1).background(Color(UIColor(hexString: "333333")))
+                }
+                
+                VStack(alignment: .leading, spacing: 10){
+                    Text("メールアドレス").foregroundStyle(.black).font(.custom("ZenMaruGothic-Regular", size: 14))
+                    TextField("", text: $inputEmail)
+                        .frame(width: 240, height: 30)
+                        .multilineTextAlignment(TextAlignment.center)
+                        .font(.custom("ZenMaruGothic-Regular", size: 15.0)).foregroundStyle(Color(UIColor(hexString: "333333"))).background(.clear)
+                        .cornerRadius(5)
+                        .overlay(
+                            RoundedRectangle(cornerSize: CGSize(width: 5.0, height: 5.0))
+                                .stroke(Color(UIColor(hexString: "333333")), lineWidth: 0.5)
+                        )
+                }
+                
+                ButtonView(action: {isMoveToHome = true}, backColor: "FDF5F3", textColor: "333333", text: "メールを送る")
+            }
+            NavigationLink(
+                destination: HomeView(),
+                isActive: $isMoveToHome,
+                label: { EmptyView() }
+            ).hidden()
+        }
+        .customBackButton()///戻るボタン
     }
 }
 
