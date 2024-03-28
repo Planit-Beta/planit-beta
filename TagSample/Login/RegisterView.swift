c4221b616ae7f7fc2db606436ff093f5b10cf598
Haruto Hamano <haruto.yhs318@gmail.com>
2024-03-28 23:54:11 +0900

    created register view

---
+++ TagSample/Login/RegisterView.swift
@@ -0,0 +1,18 @@
+//
+//  RegisterView.swift
+//  TagSample
+//
+//  Created by 濱野遥斗 on 2024/03/28.
+//
+
+import SwiftUI
+
+struct RegisterView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    RegisterView()
+}
